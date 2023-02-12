35084bfdf4df9864c54850ea1d84ac31db114d8c
dimitriasilveria <dimitriasilveria@gmail.com>
2023-02-12 15:24:47 -0300

    adding angles and coordinates calculation to the code

--- optical_flow.py
+++ optical_flow.py
@@ -1,6 +1,33 @@
 import numpy as np
 import cv2 as cv
 import argparse
+import math as mt
+
+#camera intrinsic parameters
+f = 3.67 #(mm)
+sx = 3.98 * 10**(-3) #(mm)
+sy = 3.98 * 10**(-3)
+cx = 320
+cy = 240
+
+h_cam = 60 #(cm)
+
+def get_thetas(u,v):
+    
+    x = (v-cx)*sx
+    y = (u-cy)*sy
+    theta_x = mt.atan(x/f)
+    theta_y = mt.atan(y/f)
+    if(theta_x*180/mt.pi) >= 90:
+        print('angulo invalido')
+    return theta_x, theta_y, x, y
+
+def get_3D(theta_x):
+    alpha = mt.pi - theta_x
+    Z = mt.tan(alpha) * h_cam
+    # X = Z*x/f
+    # Y = Z*y/f
+    return Z
 # parser = argparse.ArgumentParser(description='This sample demonstrates Lucas-Kanade Optical Flow calculation. \
 #                                               The example file can be downloaded from: \
 #                                               https://www.bogotobogo.com/python/OpenCV_Python/images/mean_shift_tracking/slow_traffic_small.mp4')
@@ -14,7 +41,7 @@ feature_params = dict( maxCorners = 100,
                        blockSize = 7 )
 # Parameters for lucas kanade optical flow
 lk_params = dict( winSize  = (15,15),
-                  maxLevel = 0,
+                  maxLevel = 2,
                   criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
 # Create some random colors
 color = np.random.randint(0,255,(100,3))
@@ -33,12 +60,22 @@ while(1):
     # Select good points
     good_new = p1[st==1]
     good_old = p0[st==1]
+
+    if good_new != good_old:
+        theta_x, theta_y = get_thetas(p0)
+        #depois publicamos esses valores nos servos, então calculamos as coordenadas 3D
+    
+    #depois que a bola está alinhada ao centro da câmera, calculamos as coordenadas 3D da bola
+
+    Z = get_3D(theta_x)
+
     # draw the tracks
     for i,(new,old) in enumerate(zip(good_new, good_old)):
         a,b = new.ravel()
         c,d = old.ravel()
         mask = cv.line(mask, (a,b),(c,d), color[i].tolist(), 2)
         frame = cv.circle(frame,(a,b),5,color[i].tolist(),-1)
+        frame = cv.circle(frame,(cy,cx), 15, (0,0,255), -1)
     img = cv.add(frame,mask)
     cv.imshow('frame',img)
     k = cv.waitKey(30) & 0xff
@@ -47,3 +84,6 @@ while(1):
     # Now update the previous frame and previous points
     old_gray = frame_gray.copy()
     p0 = good_new.reshape(-1,1,2)
+
+
+
